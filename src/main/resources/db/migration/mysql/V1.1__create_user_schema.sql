84d703e4f6fd96d59c1bdff5b47b8f2e39c08f5e
Bogdan Cristian Paulon <bogdan_paulon@sync.ro>
2019-04-03 15:25:51 +0300

    Update V1.1__create_user_schema.sql

--- src/main/resources/db/migration/mysql/V1.1__create_user_schema.sql
+++ src/main/resources/db/migration/mysql/V1.1__create_user_schema.sql
@@ -1,6 +1,3 @@
--- create sequence hibernate_sequence start with 1 increment by 1;
-
--- Create the users Database
 CREATE TABLE IF NOT EXISTS employee (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name varchar(20),
