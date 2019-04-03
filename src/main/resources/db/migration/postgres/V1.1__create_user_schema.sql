06ffd5fa574402527c7408566bb5b06279f53da5
Bogdan Cristian Paulon <bogdan_paulon@sync.ro>
2019-04-03 15:25:20 +0300

    Update V1.1__create_user_schema.sql

--- src/main/resources/db/migration/postgres/V1.1__create_user_schema.sql
+++ src/main/resources/db/migration/postgres/V1.1__create_user_schema.sql
@@ -1,6 +1,4 @@
--- create sequence hibernate_sequence start with 1 increment by 1;
-
--- Create the users Database
+-- automatically creates employee_id_seq and makes this table the owner of the created sequence
 CREATE TABLE IF NOT EXISTS employee (
 	id SERIAL PRIMARY KEY,
 	name varchar(20),
